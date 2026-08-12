e0328590ab33a359575c91a6d7e4977431996398
ArshilKhan09 <arshilkhan529@gmail.com>
2026-08-12 21:20:23 +0530

    Remove generated Java class files

--- Star_Patterns/Pattern1.java
+++ Star_Patterns/Pattern1.java
@@ -13,9 +13,7 @@ public class Pattern1 {
 
      public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Enter number of Rows : ");
         int row = sc.nextInt();
-        System.out.print("Enter number of Columns : ");
         int column = sc.nextInt();
         for(int i = 0 ; i < row ; i++){
             for (int j = 0 ; j < column ; j++){
