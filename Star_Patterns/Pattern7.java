ed22938bb04e56288b711f2e15090b63b12a5546
ArshilKhan09 <arshilkhan529@gmail.com>
2026-08-15 22:12:53 +0530

    clear

---
+++ Star_Patterns/Pattern7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Pattern7 {
+    /*
+    Pattern Triangle star Pattern
+         * 
+       * * * 
+     * * * * *
+   * * * * * * *
+ * * * * * * * * *
+    */
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int row = 0 ; row < n ; row++){
+            for(int column = 0 ; column < n-row+1 ; column++){
+                System.out.print(" ");
+            }
+
+            for(int star = 0 ; star < 2*row+1 ; star++){
+                System.out.print("*");
+            }
+
+            for(int column = 0 ; column < n-row+1 ; column++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
